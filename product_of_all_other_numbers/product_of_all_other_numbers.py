e10133a12a4a74c0fd09be92c3117c7179553211
unknown <prakashbahadursingh2@gmail.com>
2020-08-20 19:30:47 -0700

    moving zeros algorithm completed

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -20,10 +20,31 @@ def product_of_all_other_numbers(arr):
     # then return the  new array with products of all numbers except the current index number
     return new_array
 
+# O(n^2)
+# def product_of_all_other_numbers(arr, excluded_idx=0, new_array=[]):
+#     # base case(s)?
+#     # when the excluded index (ex_i) is greater
+#     # or equal to length of the arr
+#     if excluded_idx >= len(arr):
+#         return new_array
+    
+#     # otherwise
+#     product = 1
+
+#     # iterate through the arr
+#     for i in range(len(arr)):
+#         # if current i is not the excluded_idx
+#         if i != excluded_idx:
+#             # multiply value with product
+#             product *= arr[i]
+#     # append product to new_array
+#     new_array.append(product)
+#     # return a call to self with (arr, excluded_idx + 1, new_array)
+#     return product_of_all_other_numbers(arr, excluded_idx + 1, new_array)
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
